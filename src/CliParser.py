7e963337163364775373fbc4c05d3be002f51223
msp <msp1891@students.fhv.at>
2022-11-27 15:54:45 +0100

    Expanded CliParser.py for new parameter including unittesting.

--- src/CliParser.py
+++ src/CliParser.py
@@ -1,24 +1,54 @@
 DEFAULT_HEIGHT = 600
 DEFAULT_WIDTH =  800
+DEFAULT_X_START = -2.0
+DEFAULT_X_END = 1.0
+DEFAULT_Y_START = -1.0
+DEFAULT_Y_END = 1.0
+DEFAULT_MAX_ITER = 100
+DEFAULT_INFINITY_LIMIT = 4
 
 class Arguments:
     width: int
     height: int
+    x_start: float
+    x_end: float
+    y_start: float
+    y_end: float
+    max_iter: int
+    infinity_limit: int
 
 def parseArguments(argv : list[str]) :
     args = Arguments()
-    if (len(argv) == 1): 
-        args.width = DEFAULT_WIDTH
-        args.height = DEFAULT_HEIGHT
-    else :
-        i = 1
-        while (i < len(argv)):
-            if (argv[i] == '-h'):
-                args.height = int(argv[i + 1])
-            elif (argv[i] == '-w'):
-                args.width = int(argv[i + 1])
-            else :
-                raise Exception('unknown parameter "{}"'.format(argv[i]))
-            i += 2
+
+    args.width = DEFAULT_WIDTH
+    args.height = DEFAULT_HEIGHT
+    args.x_start = DEFAULT_X_START
+    args.x_end = DEFAULT_X_END
+    args.y_start = DEFAULT_Y_START
+    args.y_end = DEFAULT_Y_END
+    args.max_iter = DEFAULT_MAX_ITER
+    args.infinity_limit = DEFAULT_INFINITY_LIMIT
+
+    i = 1
+    while (i < len(argv)):
+        if (argv[i] == '-h'):
+            args.height = int(argv[i + 1])
+        elif (argv[i] == '-w'):
+            args.width = int(argv[i + 1])
+        elif (argv[i] == '-x_start'):
+            args.x_start = float(argv[i + 1])
+        elif (argv[i] == '-x_end'):
+            args.x_end = float(argv[i + 1])
+        elif (argv[i] == '-y_start'):
+            args.y_start = float(argv[i + 1])
+        elif (argv[i] == '-y_end'):
+            args.y_end = float(argv[i + 1])
+        elif (argv[i] == '-max_iter'):
+            args.max_iter = int(argv[i + 1])
+        elif (argv[i] == '-infinity_limit'):
+            args.infinity_limit = int(argv[i + 1])
+        else :
+            raise Exception('unknown parameter "{}"'.format(argv[i]))
+        i += 2
 
     return args
--- src/test_CliParser.py
+++ src/test_CliParser.py
@@ -11,6 +11,10 @@ def test_it_should_return_defaults() :
     # THEN we expect the default arguments
     assert args.height == CliParser.DEFAULT_HEIGHT
     assert args.width == CliParser.DEFAULT_WIDTH
+    assert args.x_start == CliParser.DEFAULT_X_START
+    assert args.x_end == CliParser.DEFAULT_X_END
+    assert args.y_start == CliParser.DEFAULT_Y_START
+    assert args.y_end == CliParser.DEFAULT_Y_END
 
 
 def test_it_should_parse_height() :
@@ -26,7 +30,7 @@ def test_it_should_parse_height() :
 
 def test_throw_exception_if_unknown_parameter() :
     #GIVEN an unknown parameter
-    argv1 = ["currentPath", "-x"]
+    argv1 = ["currentPath", "-u"]
 
     #WHEN the arguments get parsed
     with pytest.raises(Exception) as exception_info:
@@ -34,7 +38,7 @@ def test_throw_exception_if_unknown_parameter() :
 
     # THEN an exception should have been thrown
     print(exception_info.value)
-    assert str(exception_info.value) == 'unknown parameter "-x"'
+    assert str(exception_info.value) == 'unknown parameter "-u"'
 
 
 def test_if_the_width_is_input_correctly() :
@@ -70,3 +74,97 @@ def test_if_height_and_width_can_be_input_correctly() :
     # THEN that the width is correct
     assert args.height == 200
     assert args.width == 400
+
+def test_if_x_start_value_can_be_input_correctly():
+    #Given the x start Value parameter
+    argv = ["currentPath", "-x_start", "-1.3"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the x_start is correct
+    assert args.x_start == -1.3
+    assert args.x_end == CliParser.DEFAULT_X_END
+    assert args.y_start == CliParser.DEFAULT_Y_START
+    assert args.y_end == CliParser.DEFAULT_Y_END
+
+def test_if_x_end_value_can_be_input_correctly():
+    #Given the x end Value parameter
+    argv = ["currentPath", "-x_end", "0.7"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the x_end is correct
+    assert args.x_start == CliParser.DEFAULT_X_START
+    assert args.x_end == 0.7
+    assert args.y_start == CliParser.DEFAULT_Y_START
+    assert args.y_end == CliParser.DEFAULT_Y_END
+
+def test_if_y_start_value_can_be_input_correctly():
+    #Given the y start Value parameter
+    argv = ["currentPath", "-y_start", "-1.3"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the y_start is correct
+    assert args.x_start == CliParser.DEFAULT_X_START
+    assert args.x_end == CliParser.DEFAULT_X_END
+    assert args.y_start == -1.3
+    assert args.y_end == CliParser.DEFAULT_Y_END
+
+def test_if_y_end_value_can_be_input_correctly():
+    #Given the y end Value parameter
+    argv = ["currentPath", "-y_end", "1.3"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the y_end is correct
+    assert args.x_start == CliParser.DEFAULT_X_START
+    assert args.x_end == CliParser.DEFAULT_X_END
+    assert args.y_start == CliParser.DEFAULT_Y_START
+    assert args.y_end == 1.3
+    
+def test_if_max_iterations_can_be_input_correctly():
+    #Given the max Iterations parameter
+    argv = ["currentPath", "-max_iter", "50"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the max iterations is correct
+    assert args.max_iter == 50
+    assert args.infinity_limit == CliParser.DEFAULT_INFINITY_LIMIT
+
+def test_if_infinity_limit_can_be_input_correctly():
+    #Given the infinity limit parameter
+    argv = ["currentPath", "-infinity_limit", "2"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the infinity limit is correct
+    assert args.max_iter == CliParser.DEFAULT_MAX_ITER
+    assert args.infinity_limit == 2
+
+def test_if_xy_start_xy_end_hight_and_infinity_limit_value_can_be_input_correctly():
+    #Given the xy start, xy end, hight and infinity limit Value parameter
+    argv = ["currentPath", "-x_start", "-1.3", "-x_end", "0.7", "-y_start", "-0.7", "-y_end", "1.3", "-h", "300", "-infinity_limit", "6"]
+
+    #When the arguments get parsed
+    args = CliParser.parseArguments(argv)
+
+    # Then check that the xy start, xy end, hight and infinity limit value is correct
+    assert args.height == 300
+    assert args.width == CliParser.DEFAULT_WIDTH
+    assert args.x_start == -1.3
+    assert args.x_end == 0.7
+    assert args.y_start == -0.7
+    assert args.y_end == 1.3
+    assert args.infinity_limit == 6
+    assert args.max_iter == CliParser.DEFAULT_MAX_ITER
+
+
+    
