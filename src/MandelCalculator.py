d4af6bf729daaad931afc87b959c03c63e4108c8
msp <msp1891@students.fhv.at>
2022-11-27 16:41:25 +0100

    Wip creating Mandel calculator

---
+++ src/MandelCalculator.py
@@ -0,0 +1,25 @@
+
+class MandelCalculator: 
+    def __init__(self, x_start, x_delta, y_start, y_delta, max_iterations, infinity_limit) -> None:
+        self.x_start = x_start
+        self.x_delta = x_delta
+        self.y_start = y_start
+        self.y_delta = y_delta
+        self.max_iterations = max_iterations
+        self.infinity_limit = infinity_limit
+
+
+
+    def getMandelValue(self, x :int, y :int) -> int:
+
+        c = complex(x,y)
+        zn = c
+        i = 0
+        # z1 = z0**2 + c
+        # z2 = z1**2 + c
+        while((i <= self.max_iterations) or (abs(zn) < self.infinity_limit)):
+            zn = zn**2 + c
+            i = i + 1
+        return i
+        
+        #<Number of iterations until mandelbrot formula goes to infinity>
---
+++ src/test_MandelCalculator.py
@@ -0,0 +1,21 @@
+import MandelCalculator
+import pytest
+
+def test_if_max_iteration_reached_for_origin ():
+	#Given the Mandel calculator
+    calculater = MandelCalculator(-2, 3, -1, 2, 100, 4)
+    
+    #When calculating with 
+
+
+
+def test_if_condition_for_complex_number_correkt (complex_Number):
+    #Funktion to check mandelbrot correktness
+    #-----------------------------------------
+    #complex number
+    #End condition
+        #infinity or max iterrations
+    #-----------------------------------------
+    
+    
+    return #True or False
